1624c68c63756111c3ff0cad6ff21869b6a165ea
daniel-modex <danielmodex33@gmail.com>
2024-11-16 16:08:53 +0530

    3:52

--- src/app/Pages/Registeration/register-page/register-page.component.ts
+++ src/app/Pages/Registeration/register-page/register-page.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -8,11 +9,13 @@ import { timer } from 'rxjs';
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+  passwordError:boolean=false;
+  emailError:boolean=false;
 
   constructor(private toastr:ToastrService){
 
@@ -38,49 +41,54 @@ onRegister(){
   if (this.ischeck) {
     this.registerObj.role="provider"
   }
-  this.http.post("https://localhost:7001/api/AuthApi/Register",this.registerObj).subscribe((res:any)=>{
-    console.log(res)
-    if(res.isSuccessful){
-      if (this.registerObj.role=='user') {
-        this.http.post("https://localhost:5002/api/UserApi",this.registerObj).subscribe((user:any)=>{
-          console.log(user)
-          if (user.result) {
-            // alert("Successfully registered User")
-            this.toastr.success("Successfully registered as User")
-            timer(2000).subscribe(() => {
-              window.location.reload()
-            });
-          }
-        })
+  if (this.passwordError && this.emailError) {
+    this.http.post("https://localhost:7001/api/AuthApi/Register",this.registerObj).subscribe((res:any)=>{
+      console.log(res)
+      if(res.isSuccessful){
+        if (this.registerObj.role=='user') {
+          this.http.post("https://localhost:5002/api/UserApi",this.registerObj).subscribe((user:any)=>{
+            console.log(user)
+            if (user.result) {
+              // alert("Successfully registered User")
+              this.toastr.success("Successfully registered as User")
+              timer(2000).subscribe(() => {
+                window.location.reload()
+              });
+            }
+            
+          })
+        }
+        if (this.registerObj.role=='provider') {
+          this.http.post("https://localhost:7057/api/Services/Register",this.registerObj).subscribe((user:any)=>{
+            console.log(user)
+            if (user.result) {
+              // alert("Successfully registered Service Provider")
+              this.toastr.success("Successfully registered as Service Provider")
+              timer(2000).subscribe(() => {
+                window.location.reload()
+              });
+            }
+          })
+        }
+        
+        
+        
+        
+        
       }
-      if (this.registerObj.role=='provider') {
-        this.http.post("https://localhost:7057/api/Services/Register",this.registerObj).subscribe((user:any)=>{
-          console.log(user)
-          if (user.result) {
-            // alert("Successfully registered Service Provider")
-            this.toastr.success("Successfully registered as Service Provider")
-            timer(2000).subscribe(() => {
-              window.location.reload()
-            });
-          }
-        })
-      }
-      
-      
-      
-      
-      
-    }
-   else{
-    alert("Something went wrong")
-   }
-  })
+     else{
+      alert("Something went wrong")
+     }
+    })
+  }
   
 }
 
 onLogin(){
   this.http.post("https://localhost:7001/api/AuthApi/Login",this.loginObj).subscribe((res:any)=>{
     localStorage.setItem('userName',res.username)
+    localStorage.setItem('token',res.token)
+    localStorage.setItem('role',res.role)
     console.log(res)
   if (res) {
     
@@ -111,6 +119,40 @@ onLogin(){
   })
 }
 
+onEmailChange(email: string) {
+  if (email) {
+    this.validateEmail(email);
+  }
+}
 
+validateEmail(email: string): void {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailPattern.test(email)) {
+    this.toastr.error('Invalid email format');
+  }
+}
+
+validatePassword(): void {
+  const password = this.registerObj.password;
+
+  if (!password) {
+    this.toastr.error('Password is required.');
+  } else if (password.length < 6) {
+    this.toastr.error('Password must be at least 6 characters long.');
+  } else if (!/[A-Z]/.test(password)) {
+    this.toastr.error(
+      'Password must contain at least one uppercase letter.');
+  } else if (!/[a-z]/.test(password)) {
+    this.toastr.error(
+      'Password must contain at least one lowercase letter.');
+  } else if (!/\d/.test(password)) {
+    this.toastr.error('Password must contain at least one number.');
+  } else if (!/[@$!%*?&]/.test(password)) {
+    this.toastr.error(
+      'Password must contain at least one special character.');
+  } else {
+    this.passwordError = true; // No errors
+  }
+}
 
 }
