ebac73e9e89373e7383e80a81c6ea857deb1e950
Asmi675 <asmitha675@gmail.com>
2024-11-18 16:13:26 +0530

    16:13PM(18-11-2024)

--- src/app/Pages/user page/electrician-service-page/electrician-service-page.component.ts
+++ src/app/Pages/user page/electrician-service-page/electrician-service-page.component.ts
@@ -69,6 +69,22 @@ domain:string="Electrician"
     this.ProfileName = localStorage.getItem('userName')
   }
 
+  BookingDetails:any={
+    id: 0,
+    userName: "",
+    professionalName: "",
+    bookingStatus: "",
+    bookingRequestedSuccessfully: false,
+    serviceProviderResponse: false,
+    isCompleted: false,
+    serviceProvided: "",
+    serviceDetails: "",
+    price: 0,
+    createdAt: "2024-11-17T11:38:06.758Z",
+    paymetSuccessful: false,
+    cancelationConfirmed: false
+  }
+
   fetchServicesFromBackend() {
     this.http.get("https://localhost:7057/api/Services/GetProfessionalsByDomain/"+this.domain).subscribe((res:any)=>{
       console.log(res.result)
@@ -80,7 +96,32 @@ domain:string="Electrician"
   onLogOut(){
     localStorage.clear()
   }
-  Book(){
- 
+  EmailObj:any={
+    id: "",
+    email: "",
+    phone: "",
+    otp: ""
   }
+
+  Book(item:any,){
+ console.log(item)
+ this.BookingDetails.userName=localStorage.getItem('userName')
+ this.BookingDetails.professionalName = item.userName
+ this.BookingDetails.bookingStatus = "Booking Started"
+ this.BookingDetails.serviceProvided = item.domain
+ this.BookingDetails.price = item.basePay
+ console.log(this.BookingDetails)
+ this.http.post("https://localhost:7025/api/booking",this.BookingDetails).subscribe((res:any)=>{
+  console.log(res)
+  if(res.isSuccessful){
+    this.EmailObj.phone=item.phone
+    this.EmailObj.email=item.email
+    this.EmailObj.id="668920e8e86831190cc77f0e"
+    console.log(this.EmailObj)
+    this.http.post("https://localhost:7153/api/novu/send-welcome-email",this.EmailObj).subscribe((res:any)=>{
+      console.log(res)
+    })
+  }
+})
+}
 }
