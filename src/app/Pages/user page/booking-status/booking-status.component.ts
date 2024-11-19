85bfb5e74e9be241a4fa90da90d7841804cbd901
daniel-modex <danielmodex33@gmail.com>
2024-11-19 11:53:34 +0530

    12:36

--- src/app/Pages/user page/booking-status/booking-status.component.ts
+++ src/app/Pages/user page/booking-status/booking-status.component.ts
@@ -6,177 +6,188 @@ import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 
-import { RouterLink, RouterOutlet} from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
 
- selector: 'app-booking-status',
+  selector: 'app-booking-status',
 
- standalone: true,
+  standalone: true,
 
- imports: [RouterLink ,RouterOutlet,DatePipe,CommonModule,FormsModule ],
+  imports: [RouterLink, RouterOutlet, DatePipe, CommonModule, FormsModule],
 
- templateUrl: './booking-status.component.html',
+  templateUrl: './booking-status.component.html',
 
- styleUrl: './booking-status.component.css'
+  styleUrl: './booking-status.component.css'
 
 })
 
 export class BookingStatusComponent implements OnInit {
 
- constructor(private toastr:ToastrService,private cdr:ChangeDetectorRef){
-
- }
-
- ProfileName:any =""
-
- http = inject(HttpClient)
-
- ngOnInit(): void {
-
- this.ProfileName = localStorage.getItem('userName')
-
- this.getBookingDetails(this.ProfileName)
-
- }
-
- onLogOut(){
-
-  localStorage.clear()
-
- }
-
-isVisible:boolean=false
-
- BookingDetails:any =[]
-
- uniqueProfessionalNames: any =[]
-
- getBookingDetails(userName:string){
-
-  this.http.get("https://localhost:7025/api/booking/UserName/"+userName).subscribe((res:any)=>{
-
-   console.log(res)
-
-   if(res.isSuccessful){
-
-    this.BookingDetails = res.result
-
-    console.log(this.BookingDetails)
-
-    this.extractUniqueNames(this.BookingDetails)
-
-    this.BookingDetails = [...this.BookingDetails].reverse();
-
-    this.cdr.detectChanges()
-
-   }
-
-  })
-
- }
-
- extractUniqueNames(BookingDetails:any[]){
-
- const professionalNamesSet = new Set<string>();
-
- for (let i = 0; i < BookingDetails.length; i++) {
-
-  professionalNamesSet.add(this.BookingDetails[i].professionalName);
-
- }
-
- this.uniqueProfessionalNames = Array.from(professionalNamesSet);
-
- console.log(this.uniqueProfessionalNames)
-
- }
-
- Submit(){
-
- this.reviewObj.userName=this.ProfileName
-
- this.reviewObj.message=this.reviewmessage
-
- console.log(this.reviewObj)
-
- this.http.post("https://localhost:7057/api/User/Reviews/AddReview",this.reviewObj).subscribe((res:any)=>{
-
-  console.log(res)
-
-  if (res.isSuccessful) {
-
-   this.toastr.success("review successfully submitted")
+  constructor(private toastr: ToastrService, private cdr: ChangeDetectorRef) {
 
   }
 
- })
+  ProfileName: any = ""
 
- }
+  http = inject(HttpClient)
 
-reviewObj:any={
+  ngOnInit(): void {
 
- id: 0,
+    this.ProfileName = localStorage.getItem('userName')
 
- professionalName: "",
+    this.getBookingDetails(this.ProfileName)
 
- userName: "",
+  }
 
- dateTime: "2024-11-18T04:33:22.899Z",
+  onLogOut() {
 
- message: "",
+    localStorage.clear()
 
- userRating: 0
+  }
 
-}
+  isVisible: boolean = false
 
-reviewmessage:any
+  BookingDetails: any = []
 
- Review(name:string){
+  uniqueProfessionalNames: any = []
 
-this.isVisible=true
+  getBookingDetails(userName: string) {
 
-this.reviewObj.professionalName=name
+    this.http.get("https://localhost:7025/api/booking/UserName/" + userName).subscribe((res: any) => {
 
- }
+      console.log(res)
 
- responseObj:any={
+      if (res.isSuccessful) {
 
- responseId: 0,
+        this.BookingDetails = res.result
 
- bookingId: 0,
+        console.log(this.BookingDetails)
 
- responseValue: false,
+        this.extractUniqueNames(this.BookingDetails)
 
- responseMessage: ""
+        this.BookingDetails = [...this.BookingDetails].reverse();
 
- }
+        this.cdr.detectChanges()
 
- Delete(id:any){
+      }
 
- this.responseObj.bookingId=id
+    })
 
- this.responseObj.responseValue=true
+  }
 
-this.http.put("https://localhost:7025/api/booking/CancelBooking",this.responseObj).subscribe((res:any)=>{
+  extractUniqueNames(BookingDetails: any[]) {
 
- console.log(res)
+    const professionalNamesSet = new Set<string>();
 
- if(res.isSuccessful){
+    for (let i = 0; i < BookingDetails.length; i++) {
 
-  setTimeout(() => {
+      professionalNamesSet.add(this.BookingDetails[i].professionalName);
 
-   window.location.reload()
+    }
 
-  }, 5000);
+    this.uniqueProfessionalNames = Array.from(professionalNamesSet);
 
- }
+    console.log(this.uniqueProfessionalNames)
 
-})
+  }
 
- }
+  Submit() {
+
+    this.reviewObj.userName = this.ProfileName
+
+    this.reviewObj.message = this.reviewmessage
+
+    console.log(this.reviewObj)
+
+    this.http.post("https://localhost:7057/api/User/Reviews/AddReview", this.reviewObj).subscribe((res: any) => {
+
+      console.log(res)
+
+      if (res.isSuccessful) {
+
+        this.toastr.success("review successfully submitted")
+
+      }
+
+    })
+
+  }
+
+  reviewObj: any = {
+
+    id: 0,
+
+    professionalName: "",
+
+    userName: "",
+
+    dateTime: "2024-11-18T04:33:22.899Z",
+
+    message: "",
+
+    userRating: 0
+
+  }
+
+  reviewmessage: any
+  pastReviews:any
+
+  Review(name: string) {
+    this.reviewObj.professionalName = name
+      this.http.get("https://localhost:7057/api/User/Reviews/GetReviewsByName?username="+name).subscribe((res:any)=>{
+        console.log(res)
+      this.pastReviews=res.result
+      console.log(this.pastReviews)
+      this.cdr.detectChanges()
+      })
+
+    setTimeout(() => {
+      this.isVisible = true
+
+      
+    }, 1000);
+   
+
+  }
+
+  responseObj: any = {
+
+    responseId: 0,
+
+    bookingId: 0,
+
+    responseValue: false,
+
+    responseMessage: ""
+
+  }
+
+  Delete(id: any) {
+
+    this.responseObj.bookingId = id
+
+    this.responseObj.responseValue = true
+
+    this.http.put("https://localhost:7025/api/booking/CancelBooking", this.responseObj).subscribe((res: any) => {
+
+      console.log(res)
+
+      if (res.isSuccessful) {
+
+        setTimeout(() => {
+
+          window.location.reload()
+
+        }, 5000);
+
+      }
+
+    })
+
+  }
 
 }
 
